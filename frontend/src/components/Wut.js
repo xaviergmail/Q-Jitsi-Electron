a961efb9d304267d3e040908782e5228b8cb3c95
Tzikas <niko.tzikas@gmail.com>
2021-07-06 10:39:38 -0400

    about to revert

--- frontend/src/components/Wut.js
+++ frontend/src/components/Wut.js
@@ -3,38 +3,38 @@ import TheContext from '../TheContext'
 import actions from '../api'
 
 function Wut(props) {
-    console.log(props, window, window.jitsiMeetExternalAPI, '!WUT')
-    const { socket } = useContext(TheContext)
+    // console.log(props, window, window.jitsiMeetExternalAPI, '!WUT')
+    // const { socket } = useContext(TheContext)
 
 
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        const api = window.jitsiMeetExternalAPI
-        // console.log(api?._getLargeVideo(), ' get the frigginn lagrge vid', socket)
-        let feed = api?._getLargeVideo()
+    //     const api = window.jitsiMeetExternalAPI
+    //     // console.log(api?._getLargeVideo(), ' get the frigginn lagrge vid', socket)
+    //     let feed = api?._getLargeVideo()
 
-        // console.log(feed, ' feed me', api?.captureLargeVideoScreenshot())
+    //     // console.log(feed, ' feed me', api?.captureLargeVideoScreenshot())
 
-        if (!api) return
+    //     if (!api) return
 
-        setTimeout(() => {
-            api.captureLargeVideoScreenshot().then(data => {
-                // data is an Object with only one param, dataURL
-                // data.dataURL = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAABQAA..."
-                console.log(data.dataURL)
-                var bindata = new Buffer(data.dataURL.split(",")[1], "base64");
+    //     setTimeout(() => {
+    //         api.captureLargeVideoScreenshot().then(data => {
+    //             // data is an Object with only one param, dataURL
+    //             // data.dataURL = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAABQAA..."
+    //             console.log(data.dataURL)
+    //             var bindata = new Buffer(data.dataURL.split(",")[1], "base64");
 
-                actions.shareScreenShot({ shot: bindata })
-            })
+    //             actions.shareScreenShot({ shot: bindata })
+    //         })
 
-        }, 10000)
+    //     }, 10000)
 
-        //socket.emit('video', { feed })
-        // if (vid) {
-        //     socket.emit('video', vid)
-        // }
-    }, [window.jitsiMeetExternalAPI])
+    //     //socket.emit('video', { feed })
+    //     // if (vid) {
+    //     //     socket.emit('video', vid)
+    //     // }
+    // }, [window.jitsiMeetExternalAPI])
 
     // useEffect(() => {
     //   console.log(window, window.jitsiMeetExternalAPI, 'WUT')
